899e34743eef3d7d0cac03a5d185a5a07006a920
joeleger <joeleger59@gmail.com>
2025-02-19 16:37:37 -0500

    logic now works pre test checks

--- app/main.py
+++ app/main.py
@@ -20,9 +20,13 @@ def count_the_days_alt(dateValue):
     day = int(dateValue.split("-")[2])
     days: int = 0
 
-    months = list(range(1, 13))
+    months = list(range(1, month))
     days_in_the_month = [31, is_leap_year(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    combined_months_days = list(zip(months, days_in_the_month))
+    combined_months_days = dict((zip(months, days_in_the_month)))
+    sum_days = sum(v for v in combined_months_days.values())
+    print(combined_months_days)
+
+    days += sum_days + day
 
     return days
 
@@ -38,3 +42,6 @@ def is_leap_year(year: int) -> int:
             return 29
     else:
         return 28
+
+
+print(count_the_days_alt("2025-12-28"))
